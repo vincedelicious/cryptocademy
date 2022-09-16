c85f16cddd185eb38df10a029f07048af04bedec
Taneshwar Sodhi <taneshwar12@gmail.com>
2022-09-16 14:29:17 +1000

    Completed

--- app/javascript/controllers/content_swap_controller.js
+++ app/javascript/controllers/content_swap_controller.js
@@ -3,29 +3,36 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="content-swap"
 export default class extends Controller {
   static targets = ["card", "card_no"];
-  static values = ["order"];
+  static values = {
+    order: Number
+  }
+
   connect() {
     console.log("Connected");
+    this.index = 0;
+    this.cardTargets[this.index].classList.add("active-card");
   }
 
-  next(){
-    let currentCard = this.orderValue;
-    console.log(this.orderValue);
-    console.log("Up to here!");
-    const total = this.cardTargets.length;
-    if (currentCard < total) {
-      this.cardTargets[currentCard + 1].classList.add("active-card");
-      console.log(this.cardTarget);
-    } else {
+  next(e){
+    e.preventDefault();
+    this.cardTargets[this.index].classList.remove("active-card");
+    this.index = this.index + 1;
+    this.cardTargets[this.index].classList.add("active-card");
+    // if (currentCard < total) {
+    //   this.cardTargets[currentCard + 1].classList.add("active-card");
+    //   console.log(this.cardTarget);
+    // } else {
 
-    }
+    // }
   }
 
-  back(){
-    let currentCard = this.card_noTarget;
-    const total = this.cardTargets.length;
-    if (currentCard != 0 ) {
-      this.cardTarget.classList.remove("active-card");
-    }
+  back(e){
+    e.preventDefault();
+    this.cardTargets[this.index].classList.remove("active-card");
+    this.index = this.index -1;
+    this.cardTargets[this.index].classList.add("active-card");
+    // if (currentCard != 0 ) {
+    //   this.cardTarget.classList.remove("active-card");
+    // }
   }
 }
